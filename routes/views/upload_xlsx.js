a00400224941264e0d7ffe38fa104d038194831d
panjizhi <panjizhi1987@gmail.com>
2017-08-28 15:33:54 +0800

    update

--- routes/views/upload_xlsx.js
+++ routes/views/upload_xlsx.js
@@ -1,22 +1,105 @@
 var keystone = require('keystone');
+var Excel = require('exceljs');
 var Question = keystone.list('Question');
 
 exports = module.exports = function (req, res) {
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+    var view = new keystone.View(req, res);
+    var locals = res.locals;
 
-	// Set locals
-	locals.section = 'uploadXLSX';
-	locals.submitted = false;
+    // Set locals
+    locals.section = 'uploadXLSX';
+    locals.submitted = false;
 
-	// On POST requests, add the Enquiry item to the database
-	view.on('post', { action: 'import' }, function (next) {
-		console.log(req);
-		locals.submitted = true;
-		
-		next();
-	});
+    // On POST requests, add the Enquiry item to the database
+    view.on('post', { action: 'import' }, function (next) {
+        xlsx2json(req.files.file.path).then(json => {
+            console.log(json);
+            
+            locals.submitted = true;
+            next();
+        });
+    });
 
-	view.render('upload_xlsx');
+    view.render('upload_xlsx');
 };
+
+function xlsx2json(filename) {
+    let sheets = [];
+
+    return new Excel.Workbook().xlsx.readFile(filename).then(worksheets => {
+        worksheets.eachSheet(worksheet => {
+            worksheet.eachRow((row, rowNumber) => {
+                if ( rowNumber === 1 ) {
+                    return;
+                }
+
+                let item = {
+                    type: '',
+                    question: '',
+                    options: [],
+                    answer: ''
+                };
+
+                row.eachCell({
+                    includeEmpty: true
+                }, (cell, colNumber) => {
+                    let text = _text(cell.value);
+
+                    switch(colNumber) {
+                        case 1:
+                            item.type = text;
+                            break;
+                        case 2:
+                            item.question = text;
+                            break;
+                        case 3:
+                            text && item.options.push(`A. ${text}`);
+                            break;
+                        case 4:
+                            text && item.options.push(`B. ${text}`);
+                            break;
+                        case 5:
+                            text && item.options.push(`C. ${text}`);
+                            break;
+                        case 6:
+                            text && item.options.push(`D. ${text}`);
+                            break;
+                        case 7:
+                            text && item.options.push(`E. ${text}`);
+                            break;
+                        case 8:
+                            item.answer = text.split('').join(',');
+                            break;
+                    }
+
+                    sheets.push(item);
+                });
+            });
+        });
+
+        return sheets;
+    });
+}
+
+function _isObject(obj) {
+    return Object.prototype.toString.call(obj) === '[object Object]';
+}
+
+function _text(value) {
+    if ( typeof value === 'string' || !value ) {
+        return value;
+    }
+
+    if ( _isObject(value) && value.richText ) {
+        let pText = '';
+
+        (value.richText || []).forEach( item => {
+            pText += item.text;
+        });
+
+        return pText;
+    }
+
+    return JSON.stringify(value);
+}
